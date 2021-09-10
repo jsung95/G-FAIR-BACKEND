c09f16a6ad5b9c3fdc0a8f8d11b2e776443e8b09
Dan <wondada91@gmail.com>
2021-09-10 17:22:35 +0900

    danny
    service 합침, PhotoVO2, PreApplicationVO DTO

--- src/main/java/com/korea/gfair/service/ReplyServiceImpl.java
+++ src/main/java/com/korea/gfair/service/ReplyServiceImpl.java
@@ -4,6 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.zerock.myapp.domain.Criteria;
+import org.zerock.myapp.domain.ReplyPageDTO;
+import org.zerock.myapp.mapper.ReplyMapper;
 
 import com.korea.gfair.domain.ReplyDTO;
 import com.korea.gfair.domain.ReplyVO;
@@ -45,4 +48,77 @@ public class ReplyServiceImpl implements ReplyService {
 		return this.dao.delete(dto);
 	}//removeReply
 
+	
+	//-------------- danny ----------------//
+	@Setter(onMethod_= {@Autowired})
+	private ReplyMapper replyMapper;
+	
+	
+	//=========================== 1. register ==========================//
+	@Override
+	public int register(ReplyDTO dto) {
+		log.debug("register(dto) invoked.");
+		log.info("\t+ dto: " + dto);
+		
+		return this.replyMapper.insert(dto);
+	}//register()
+
+	
+	
+	//=========================== 2. get ==========================//
+	@Override
+	public ReplyVO get(Integer reno) {
+		log.debug("get(reno) invoked.");
+		log.info("\t+ dto: " + reno);
+		
+		return this.replyMapper.read(reno);
+	}//get()
+	
+	
+	
+	//=========================== 3. modify ==========================//
+	@Override
+	public int modify(ReplyDTO dto) {
+		log.debug("modify(reno) invoked.");
+		log.info("\t+ dto: " + dto);
+		
+		return this.replyMapper.update(dto);
+	}//modify()
+	
+	
+	
+	//=========================== 4. remove ==========================//
+	@Override
+	public int remove(Integer reno) {
+		log.debug("remove(dto) invoked.");
+		log.info("\t+ dto: " + reno);
+		
+		return this.replyMapper.delete(reno);
+	}//remove()
+	
+	
+	
+	//=========================== 5. getList ==========================//
+	@Override
+	public List<ReplyVO> getList(Criteria cri, Integer bno) {
+		log.debug("getList(cri, bno) invoked.");
+		log.info("\t+ cri: {}, bno: {} " + cri, bno);
+		
+		
+		return this.replyMapper.getListWithPaging(cri, bno);
+	}
+
+
+
+	//=========================== 5. getListPerPage ==========================//
+	@Override
+	public ReplyPageDTO getListPerPage(Criteria cri, Integer bno) {
+		log.debug("getListPerPage(cri, bno) invoked.");
+		log.info("\t+ cri: {}, bno: {}", cri, bno);
+		
+		return new ReplyPageDTO(
+				this.replyMapper.getCountByBno(bno),
+				this.replyMapper.getListWithPaging(cri, bno)
+			);
+	}//getList()
 }//end class
