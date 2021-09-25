7cfb3c9d942b0903323514129b2485eba5a56ce8
daninoel <80461806+daninoel@users.noreply.github.com>
2021-09-25 20:19:24 +0900

    apply/app

--- src/main/webapp/resources/js/apply_app.js
+++ src/main/webapp/resources/js/apply_app.js
@@ -9,7 +9,7 @@ $(function () {
         console.debug('agreeBtn on click event triggered..');
 
         if(document.querySelector('#agree').checked){
-            let form = $('form');
+            let form = $('#appForm');
             form.submit();
         }else{
             alert('약관에 동의를 해야지만 신청서 작성이 가능합니다. ');
